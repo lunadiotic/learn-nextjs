8044a34f323eff833cc247e073aa5537c4ce5069
aimeliala <idstack@outlook.co.id>
2023-11-24 00:47:56 +0700

    memanfaatkan color pallete tailwindcss

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 
 export default function Layout({ children }) {
@@ -7,26 +7,9 @@ export default function Layout({ children }) {
       <head>
         <title>Learn Next.js Fundamental</title>
       </head>
-      <body className="flex flex-col px-4 py-4 min-h-screen">
+      <body className="bg-gray-100 flex flex-col px-4 py-4 min-h-screen">
         <header>
-          <nav>
-            <ul className="flex gap-2">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/blog">Blog</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-              <li>
-                <Link href="/contact" prefetch={false}>
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <Navbar />
         </header>
         <main className="py-3 grow">{children}</main>
         <footer className="border-t py-3 text-center text-xs">
