78a9e1f1ce9cb342e076341d9c85b8180dbb5d1b
aimeliala <idstack@outlook.co.id>
2023-11-24 22:41:17 +0700

    menggunakan static assets

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -4,6 +4,13 @@ export default function PostPage() {
   return (
     <>
       <Heading>Belajar Next.js</Heading>
+      <img
+        src="/images/image-1.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>Halaman dari blog</p>
     </>
   );
--- app/blog/latihan-route-nextjs/page.jsx
+++ app/blog/latihan-route-nextjs/page.jsx
@@ -4,6 +4,13 @@ export default function PostPage() {
   return (
     <>
       <Heading>Latihan Route Next.js</Heading>
+      <img
+        src="/images/image-2.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>Halaman dari blog</p>
     </>
   );
