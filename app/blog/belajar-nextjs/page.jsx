a4d2f613f2bd6516d42acee78821eabbbf3bf765
aimeliala <idstack@outlook.co.id>
2023-11-27 00:15:59 +0700

    mengenal data meta dari markdown - front-matter

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -1,16 +1,24 @@
 import Heading from "@/components/Heading";
+import matter from "gray-matter";
 import { marked } from "marked";
 import { readFile } from "node:fs/promises";
 
 export default async function PostPage() {
   const text = await readFile("./content/blog/belajar-nextjs.md", "utf8");
-  const html = marked(text);
+  const {
+    content,
+    data: { title, image, date, author },
+  } = matter(text);
+  const html = marked(content);
 
   return (
     <>
-      <Heading>Belajar Next.js</Heading>
+      <Heading>{title}</Heading>
+      <p className="italic text-sm pb-2">
+        {date} - {author}
+      </p>
       <img
-        src="/images/image-1.jpg"
+        src={image}
         alt=""
         width={640}
         height={360}
