dec6c9664d845cde5c269bc091ece147eda7533c
aimeliala <idstack@outlook.co.id>
2023-11-26 18:36:08 +0700

    create single post desain later

--- app/blog/layout.jsx
+++
@@ -1,8 +0,0 @@
-export default function BlogLayout({ children }) {
-  return (
-    <div className="flex">
-      <div className="">[Sidebar]</div>
-      <div className="px-4">{children}</div>
-    </div>
-  );
-}
