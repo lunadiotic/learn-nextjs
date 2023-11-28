8ec33949f1de90e01db59ad66772fb0caf3fbc46
aimeliala <idstack@outlook.co.id>
2023-11-28 12:15:23 +0700

    menampilkan data post pada halaman blog

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -4,29 +4,21 @@ import { getAllPosts } from "@/lib/post";
 
 export default async function BlogPage() {
   const posts = await getAllPosts();
-  console.log(posts);
   return (
     <>
       <Heading>Blog</Heading>
       <h2 className="text-2xl mb-3">List of posts</h2>
 
-      <PostCard
-        title="Belajar Next.js"
-        href="/blog/belajar-nextjs"
-        image="/images/image-1.jpg"
-        description="Belajar Next.js panjang"
-        date="13.01.2022"
-        author="Admin"
-      />
-
-      <PostCard
-        title="Belajar Next.js"
-        href="/blog/belajar-nextjs"
-        image="/images/image-1.jpg"
-        description="Belajar Next.js panjang"
-        date="13.01.2022"
-        author="Admin"
-      />
+      {posts.map((post) => (
+        <PostCard
+          title={post.title}
+          href=""
+          image={post.image}
+          description=""
+          date={post.date}
+          author={post.author}
+        />
+      ))}
     </>
   );
 }
