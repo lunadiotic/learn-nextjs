4acb23cda8dac90c1855f12b2d7d383d797b888f
aimeliala <idstack@outlook.co.id>
2023-11-29 11:44:42 +0700

    membuat fungsi copy link dengan client component

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useState } from "react";
+
 export default function ShareLinkButton() {
+  const [copied, setCopied] = useState(false);
   function handleClick() {
-    console.log("clicked!");
+    navigator.clipboard.writeText(window.location.href);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
   }
 
   return (
@@ -10,7 +15,7 @@ export default function ShareLinkButton() {
       onClick={handleClick}
       className="border px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700"
     >
-      Copy Link
+      {copied ? "Link Copied" : "Share Link"}
     </button>
   );
 }
