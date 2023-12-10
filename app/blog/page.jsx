3030813ab730672dd73a728b76a8d05aef1b1c42
aimeliala <idstack@outlook.co.id>
2023-12-10 09:17:38 +0700

    mendapatkan data dari query parameter untuk pagination

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,6 +1,7 @@
 import Heading from "@/components/Heading";
 import PostCard from "@/components/PostCard";
 import { getAllPosts } from "@/lib/post";
+import Link from "next/link";
 
 export const revalidate = 30;
 
@@ -8,13 +9,18 @@ export const metadata = {
   title: "Blog",
 };
 
-export default async function BlogPage() {
+export default async function BlogPage({ searchParams }) {
+  const page = parsePageParam(searchParams.page);
   const posts = await getAllPosts();
   return (
     <>
       <Heading>Blog</Heading>
       <h2 className="text-2xl mb-3">List of posts</h2>
-
+      <div className="flex gap-3 pb-3">
+        <Link href={`/blog?page=${page - 1}`}>&lt;</Link>
+        <span>Page {page}</span>
+        <Link href={`/blog?page=${page + 1}`}>&gt;</Link>
+      </div>
       {posts.map((post, index) => (
         <PostCard
           key={index}
@@ -28,4 +34,14 @@ export default async function BlogPage() {
       ))}
     </>
   );
+}
+
+function parsePageParam(paramValue) {
+  if (paramValue) {
+    const page = parseInt(paramValue);
+    if (isFinite(page) && page > 0) {
+      return page;
+    }
+  }
+  return 1;
 }
